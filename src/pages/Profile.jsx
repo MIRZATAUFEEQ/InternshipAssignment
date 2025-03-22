eeb4d5857c655dfea1772ad2e810ef22127c7cd6
Mirza <mirzataufeeq98436@gmail.com>
2025-03-22 21:04:43 +0530

    update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,7 +5,7 @@ const Profile = () => {
     return (
         <>
             <div className='border-b-2 border-dotted border-gray-500 px-5 flex flex-col gap-5'>
-                <h1 className='w-[375px] h-[68px] flex items-center '>Account Setting</h1>
+                <h1 className='w-[375px] h-[68px] flex items-center font-bold '>Account Setting</h1>
                 <div className='flex gap-6'>
                     <div className='w-[76px] h-[76px] border rounded-full relative '>
                         <img className='h-full w-full' src={profile} alt="" />
